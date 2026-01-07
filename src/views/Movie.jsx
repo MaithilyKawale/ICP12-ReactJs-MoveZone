40c6731a7b05b8aa2a3e71c20d060e6292d4963c
siddhikharde <siddhikharde2005@gmail.com>
2026-01-07 20:20:51 +0530

    seprate component created for SelectBox

--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -2,22 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 import movies from '../data/movies'
 import { Clapperboard } from 'lucide-react'
-
+import SelectBox from "../components/SelectBox"
 import MovieCard from '../components/MovieCard'
-const SelectBox = ({ value, onChange, options }) => {
-  return (
-    <select className='border  text-lg! px-5 py-2 w-[100%] cursor-pointer md:w-[25%] bg-[#234C6A]  focus:outline-none rounded-[4px] border-[1.5px] text-gray-300 border-[#456882] '
-      value={value} onChange={onChange}>
-      {
-        options.map((item) => {
-          return (
-            <option key={item} value={item} className="px-5 bg-[#456882]">{item}</option>
-          )
-        })
-      }
-    </select>
-  )
-}
+
 
 function Movie() {
   const [searchMovies, setSearchMovies] = useState("");
