ff3875a0e6b7b77f71ce194932c8c99b72ea5456
siddhikharde <siddhikharde2005@gmail.com>
2026-01-07 16:35:44 +0530

    images added for card creation

--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 const SelectBox = ({ value, onChange, options }) => {
   return (
-    <select className='border px-5 py-2 w-[100%] cursor-pointer md:w-[25%] bg-[#234C6A] focus:outline-none rounded-[4px] border-[1.5px] text-gray-300 border-[#456882] '
+    <select className='border px-5 py-2 w-[100%] cursor-pointer md:w-[25%] bg-[#234C6A]  focus:outline-none rounded-[4px] border-[1.5px] text-gray-300 border-[#456882] '
       value={value} onChange={onChange}>
       {
         options.map((item) => {
